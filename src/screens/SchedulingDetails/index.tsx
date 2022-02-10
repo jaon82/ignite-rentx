6cc087ff23260b448f673b5ec8bc89bb2a406656
Jean Nunes <jean.nunes@indt.org.br>
2022-02-10 13:29:12 -0400

    Scheduling detail

---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components';
+import BackButton from '../../components/BackButton';
+import ImagesSlider from '../../components/ImagesSlider';
+import Acessory from '../../components/Acessory';
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import {
+  Acessories,
+  Brand,
+  CalendarIcon,
+  CarImages,
+  Container,
+  Content,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Description,
+  Details,
+  Footer,
+  Header,
+  Name,
+  Period,
+  Price,
+  Rent,
+  RentalPeriod,
+  RentalPrice,
+  RentalPriceDetails,
+  RentalPriceLabel,
+  RentalPriceQuota,
+  RentalPriceTotal,
+} from './styles';
+import Button from '../../components/Button';
+
+export default function SchedulingDetails() {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+      <CarImages>
+        <ImagesSlider
+          imagesUrl={[
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYgIjNZsopctbwuHDcAKkyXHfV3Lx5vsYt2A&usqp=CAU',
+          ]}
+        />
+      </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>La</Brand>
+            <Name>a</Name>
+          </Description>
+          <Rent>
+            <Period>Aod</Period>
+            <Price>1</Price>
+          </Rent>
+        </Details>
+        <Acessories>
+          <Acessory name="380Km/h" icon={SpeedSvg} />
+          <Acessory name="3.2s" icon={AccelerationSvg} />
+          <Acessory name="800 MP" icon={ForceSvg} />
+          <Acessory name="Gasolina" icon={GasolineSvg} />
+          <Acessory name="Auto" icon={ExchangeSvg} />
+          <Acessory name="2 pessoas" icon={PeopleSvg} />
+        </Acessories>
+        <RentalPeriod>
+          <CalendarIcon>
+            <Feather
+              name="calendar"
+              size={RFValue(24)}
+              color={theme.colors.shape}
+            />
+          </CalendarIcon>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>18/06/2021</DateValue>
+          </DateInfo>
+          <Feather
+            name="chevron-right"
+            size={RFValue(10)}
+            color={theme.colors.text}
+          />
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+        <RentalPrice>
+          <RentalPriceLabel>TOTAL</RentalPriceLabel>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+      </Content>
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
