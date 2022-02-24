d280edacad8327b68cabafb1f0d740213e3a22c3
Jean Nunes <jean.nunes@indt.org.br>
2022-02-23 21:04:50 -0400

    Form validation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import {
+  Alert,
   Keyboard,
   KeyboardAvoidingView,
   StatusBar,
   TouchableWithoutFeedback,
 } from 'react-native';
+import * as Yup from 'yup';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PasswordInput from '../../components/PasswordInput';
@@ -15,6 +17,27 @@ import { Footer, Container, Header, SubTitle, Title, Form } from './styles';
 export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('A senha é obrigatória'),
+      });
+
+      await schema.validate({ email, password });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        return Alert.alert('Erro de validação', error.message);
+      }
+      Alert.alert(
+        'Erro na autenticação',
+        'Ocorreu um erro ao fazer login, verifique as credenciais',
+      );
+    }
+  }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
@@ -57,7 +80,7 @@ export default function SignIn() {
             />
             <Button
               title="Login"
-              onPress={() => {}}
+              onPress={handleSignIn}
               enabled={false}
               loading={false}
               color={theme.colors.background_secondary}
